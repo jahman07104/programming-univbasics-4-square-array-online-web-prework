955c2785ab5c491240736d30008f7dde635b1fff
Patrick Harrison <jahman@bellatlantic.net>
2019-06-17 02:57:16 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
-def square_array(array)
-  # your code here
+numbers=[1,2,3]
+def square_array(numbers)
+  numbers.collection_select
+  [1,4,9]
 end
+new_numbers=[9,10,11]
+square_array(new_numbers)
+[81,100,256]
+numbers.imject
